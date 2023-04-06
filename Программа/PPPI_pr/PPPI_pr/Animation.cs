ab9ac3be5ac22560fc58d5244e0c2f73dbb90362
Ruslan Samoilenko <seltia0203@gmail.com>
2023-04-07 02:17:31 +0300

    Создание класса Animation( анимации)

---
+++ Программа/PPPI_pr/PPPI_pr/Animation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_pr
+{
+    /// <summary>
+    /// Создание класса Анимации
+    /// </summary>
+    class Animation
+    {
+    }
+}
