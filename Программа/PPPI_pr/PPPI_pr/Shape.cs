d838a4e44b5e71697269caa2bb372f29ea841288
Ruslan Samoilenko <seltia0203@gmail.com>
2023-04-07 02:11:55 +0300

    Добавлен метод Cube(куб)

--- Программа/PPPI_pr/PPPI_pr/Shape.cs
+++ Программа/PPPI_pr/PPPI_pr/Shape.cs
@@ -7,12 +7,23 @@ using System.Threading.Tasks;
 namespace PPPI_pr
 {
     /// <summary>
-    /// Создание класса Shape(фигура)
+    /// Создание класса Shape(фигура), длина, высота, ширина
     /// </summary>
     class Shape
     {
+        
         double length;
         double width;
         double height;
+    }
+
+    /// <summary>
+    /// Метод создания куба
+    /// </summary>
+    /// <param name="_length"></param>
+    /// <param name="_width"></param>
+    /// <param name="_height"></param>
+    public void Cube(double _length, double _width, double _height) { 
+    
     }
 }
